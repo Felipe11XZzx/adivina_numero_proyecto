f78a74410f8af6a05e54cdf97c603b4d419f5b57
FelipeOrtiz <ortiz15243u@gmail.com>
2025-01-24 14:16:00 +0100

    Añadidas dependencias y avance en el JS.

--- Proyecto1/script.js
+++ Proyecto1/script.js
@@ -1,1 +1,28 @@
-'use strict';
+// Calcular número aleatorio entre 1 y 20
+
+const INITIAL_SCORE = 20
+const MAX_NUMBER = 20
+
+function initData() {
+  let score = INITIAL_SCORE
+  let highScore = 0
+  let number = Math.trunc(Math.random() * MAX_NUMBER) + 1
+}
+
+const messageField = document.querySelector(".message")
+const scoreField = document.querySelector(".score")
+const highScoreField = document.querySelector(".highscore")
+const numberField = document.querySelector(".number")
+const checkButton = document.querySelector(".check")
+const againButton = document.querySelector(".again")
+
+
+console.log(messageField)
+console.log(scoreField)
+console.log("Me cago " + scoreField.textContent)
+console.log(highScoreField)
+
+console.log("En: " + highScoreField.textContent)
+console.log("La: " + numberField.textContent)
+console.log.log("Fucking: " + checkButton.textContent)
+console.log("Puta: " + againButton.textContent)
