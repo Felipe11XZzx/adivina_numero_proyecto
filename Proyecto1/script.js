e3f63a9a42c54b1bd9b3fab09f988bf0ce2fa493
FelipeOrtiz <ortiz15243u@gmail.com>
2025-01-24 14:23:55 +0100

    Proyecto para hacer en casa.

--- Proyecto1/script.js
+++ Proyecto1/script.js
@@ -3,19 +3,21 @@
 const INITIAL_SCORE = 20
 const MAX_NUMBER = 20
 
+initData()
+
 function initData() {
   let score = INITIAL_SCORE
   let highScore = 0
   let number = Math.trunc(Math.random() * MAX_NUMBER) + 1
 }
 
+// Seleccionar todos los elementos del arbol DOM que necesitamos.
 const messageField = document.querySelector(".message")
 const scoreField = document.querySelector(".score")
 const highScoreField = document.querySelector(".highscore")
 const numberField = document.querySelector(".number")
 const checkButton = document.querySelector(".check")
 const againButton = document.querySelector(".again")
-
 
 console.log(messageField)
 console.log(scoreField)
@@ -26,3 +28,14 @@ console.log("En: " + highScoreField.textContent)
 console.log("La: " + numberField.textContent)
 console.log.log("Fucking: " + checkButton.textContent)
 console.log("Puta: " + againButton.textContent)
+
+checkButton.addEventListener("click", checkNumber)
+
+function checkNumber() {
+  // Obtenemos el número pulsado.
+  // Si no es número que lo corrija.
+  // Si es un número y no es correcto. -> Xomprobamos score: ¿Perdemos partida?
+  // Si es un número y es correcto. -> Actualizamos nuestras variables y el DOM.
+
+  console.log("Ahora comprobaríamos el número")
+}
